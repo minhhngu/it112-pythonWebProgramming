6653126f92ed5544a8b346745979980308e85db9
minhhngu <56049328+minhhngu@users.noreply.github.com>
2021-01-09 22:34:12 -0800

    Add files via upload

---
+++ assignment10-moreTests/club/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Meeting, Minute, Resource, Event
+
+
+class ResourceForm(forms.ModelForm):
+    class Meta:
+        model = Resource
+        fields = '__all__'
+
+
+class MeetingForm(forms.ModelForm):
+    class Meta:
+        model = Meeting
+        fields = '__all__'
